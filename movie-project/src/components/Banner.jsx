4c84c9fb9750738e107b04d9dc2a7c04dbac4f3f
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2024-07-24 01:42:50 +0700

    test

--- movie-project/src/components/Banner.jsx
+++ movie-project/src/components/Banner.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const Banner = () => {
   return (
-    <div className='w-full h-[800px] bg-banner bg-center bg-no-repeat bg-cover relative'>
+    <div className='w-full h-[600px] bg-banner bg-center bg-no-repeat bg-cover relative'>
       <div className='w-full h-full absolute top-0 left-0 bg-black opacity-40' />
       <div className='w-full h-full flex items-center'>
             <div>Content</div>
